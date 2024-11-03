1ae46aaced9ee9b614ade817a3d0f07e2867d0f8
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-04 00:59:47 +0800

    setup vue router, configure some dummy views to test router

--- frontend-neonella/src/App.vue
+++ frontend-neonella/src/App.vue
@@ -1,14 +1,17 @@
-<script setup>
-import { useCounterStore } from './store/counter'
-
-const counterStore = useCounterStore()
-</script>
+<script setup></script>
 
 <template>
-  <div>{{ counterStore.count }}</div>
-  <button @click="counterStore.increment" class="bg-black px-4 py-2 text-white">
-    Increment
-  </button>
+  <header>
+    <!-- Header -->
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+
+  <footer>
+    <!-- Footer -->
+  </footer>
 </template>
 
 <style lang="scss" scoped></style>
--- frontend-neonella/src/main.js
+++ frontend-neonella/src/main.js
@@ -1,9 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import router from './router'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 import App from './App.vue'
 
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
