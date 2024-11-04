56bae50f61c925a0972ebf520c77a8bfe2461125
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-04 16:35:49 +0800

    vue-admin: setup pinia, tested and working

--- backend-neonella/vue-admin/src/App.vue
+++ backend-neonella/vue-admin/src/App.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from './store/counter'
+
+const counterStore = useCounterStore()
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  {{ counterStore.count }}
+  <button
+    @click="counterStore.increment"
+    class="rounded bg-purple-500 px-4 py-2 text-white"
+  >
+    Increment
+  </button>
 </template>
