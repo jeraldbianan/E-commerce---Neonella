847c4966ae5812adaa02bde7fa127e3b86807e6d
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-04 17:12:56 +0800

    vue-admin: develop login form, install dev dependencies headlessui, heroicons, and tailwindcss forms

---
+++ backend-neonella/vue-admin/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginForm from '@/components/LoginForm.vue'
+</script>
+
+<template>
+  <div class="mt-10 flex justify-center">
+    <LoginForm />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
