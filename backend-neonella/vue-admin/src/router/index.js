7c9194f0b7beeff548cb2215d7623feb91849ad2
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-05 16:16:33 +0800

    edit prettierrc config semi to true to provde semi colons in javascript files at the end of each logic line

--- backend-neonella/vue-admin/src/router/index.js
+++ backend-neonella/vue-admin/src/router/index.js
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AppLayout from '@/components/AppLayout.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import AppLayout from '@/components/AppLayout.vue';
 
-import Login from '@/views/LoginView.vue'
-import Dashboard from '@/views/DashboardView.vue'
-import RequestPasswordReset from '@/views/RequestPasswordResetView.vue'
-import ResetPassword from '@/views/ResetPasswordView.vue'
+import Dashboard from '@/views/DashboardView.vue';
+import Products from '@/views/ProductsView.vue';
+import Users from '@/views/UsersView.vue';
+import Reports from '@/views/ReportsView.vue';
+
+import Login from '@/views/LoginView.vue';
+import RequestPasswordReset from '@/views/RequestPasswordResetView.vue';
+import ResetPassword from '@/views/ResetPasswordView.vue';
 
 const routes = [
   {
@@ -20,6 +24,21 @@ const routes = [
         path: 'dashboard',
         name: 'app.dashboard',
         component: Dashboard,
+      },
+      {
+        path: 'products',
+        name: 'app.products',
+        component: Products,
+      },
+      {
+        path: 'users',
+        name: 'app.users',
+        component: Users,
+      },
+      {
+        path: 'reports',
+        name: 'app.reports',
+        component: Reports,
       },
     ],
   },
@@ -38,11 +57,11 @@ const routes = [
     name: 'resetPassword',
     component: ResetPassword,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
