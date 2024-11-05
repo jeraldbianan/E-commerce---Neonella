e9298a3d2140d9bfe3e027d5e1952836f773653e
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-05 23:26:39 +0800

    vue-admin: develop 404 NotFound page

---
+++ backend-neonella/vue-admin/src/views/NotFoundView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import CustomButton from "@/components/ui/CustomButton.vue";
+</script>
+
+<template>
+  <section class="flex h-screen items-center justify-center">
+    <div class="flex justify-start">
+      <h1
+        class="flex h-24 items-center border-r-2 border-r-secondary px-6 text-7xl font-black text-accent"
+      >
+        404
+      </h1>
+      <div class="pl-8">
+        <h2 class="mb-4 text-5xl font-bold">Page not found</h2>
+        <p class="text-gray-600">Please check the URL and try again.</p>
+
+        <div class="mt-10">
+          <CustomButton
+            @click="() => $router.push({ name: 'app.dashboard' })"
+            type="button"
+            class="px-4 py-2"
+            aria-label="Go back home"
+          >
+            Go back home
+          </CustomButton>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
