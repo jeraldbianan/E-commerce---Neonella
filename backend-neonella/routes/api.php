fec0512815bdb328da1e23cd317fc23d7be66a12
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-06 23:49:13 +0800

    backend: refactor user route and instead of passing all user data, used UserResource to limit data passed

--- backend-neonella/routes/api.php
+++ backend-neonella/routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', [AuthController::class, 'getUser']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
