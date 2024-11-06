4ba6448de7d02a3938df48d299e837c7236ba517
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-06 17:35:09 +0800

    backend: created AuthController and register login and logout methods in api routes

---
+++ backend-neonella/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller {
+    public function login(Request $request) {
+        $validatedCredentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'remember' => ['boolean'],
+        ]);
+
+        $remember = $validatedCredentials['remember'] ?? false;
+
+        unset($validatedCredentials['remember']);
+
+        if (!Auth::attempt($validatedCredentials, $remember)) {
+            return response([
+                'message' => 'Invalid credentials',
+            ], 422);
+        }
+
+        $user = $request->user();
+        if (!$user->is_admin) {
+            Auth::logout();
+            return response([
+                'message' => 'Unauthorized Access',
+            ], 403);
+        }
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request) {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response('', 204);
+    }
+}
